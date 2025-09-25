c8bd4daaa89d7e9c272f3d0987d1330b98466b02
Dean B <deantburrows88@gmail.com>
2025-09-25 12:11:00 +0200

    Add event-driven entity purchase and upgrade system
    Introduces a unified ReplicaData module for player state management on the client, refactors HUD and EntityDetails to use state change signals, and implements server-side event listeners for entity purchase and upgrade actions. Adds new remote events for entity transactions, updates Boat class to handle these events, and initializes event listeners during game startup. Also updates type definitions and event registration logic to support the new system.

--- src/client/HUD/EntityDetails.client.lua
+++ src/client/HUD/EntityDetails.client.lua
@@ -1,5 +1,7 @@
 local Players = game:GetService("Players")
 local ReplicatedStorage = game:GetService("ReplicatedStorage")
+local StarterPlayer = game:GetService("StarterPlayer")
+local ReplicaData = require(StarterPlayer.StarterPlayerScripts.Client.Modules.ReplicaData)
 local BoatType = require(ReplicatedStorage.Shared.Types.Classes.BoatType)
 local BuildingType = require(ReplicatedStorage.Shared.Types.Classes.BuildingType)
 local PortStorageType = require(ReplicatedStorage.Shared.Types.Classes.PortStorageType)
@@ -9,8 +11,19 @@ local Events = require(ReplicatedStorage.Shared.Events.Events)
 local CalculateProgress = require(ReplicatedStorage.Shared.Utils.CalculateProgress)
 local DeepWait = require(ReplicatedStorage.Shared.Utils.DeepWait)
 local FormatNumber = require(ReplicatedStorage.Shared.Utils.FormatNumber)
+
 local player = Players.LocalPlayer
 local entityDetailsUI: Frame = DeepWait(player.PlayerGui, "EntityDetails", "DetailsContainer")
+
+local currentState = {}
+
+-- Get PlayerState Updates
+ReplicaData.StateChanged:Connect(function(playerData)
+	if playerData then
+		currentState = playerData
+		populateDetailsUI()
+	end
+end)
 
 local details = {}
 -- Defaults
@@ -68,8 +81,11 @@ function handleClosingEntityDetailsUI()
 	end
 end
 
+-- //TODO - Handle button state based on if the player can afford the upgrade/purchase
+
 -- Populate the Details Page
 function populateDetailsUI()
+	if not entityData then return end
 	local class: BoatType.BoatProps | TenderType.TenderProps | PortStorageType.StorageProps | BuildingType.BuildingProps = entityData
 
 	-- Hide all elements by defaults except for progress bars
@@ -96,6 +112,16 @@ function populateDetailsUI()
 	-- Upgrade Button
 	details.upgradeButton.TextLabel.Text = class.isPurchased and "Upgrade" or "Purchase"
 	details.upgradeButton.Visible = true
+	if currentState.Currencies and currentState.Currencies.Gold then
+		local cost = class.isPurchased and class.UpgradeCost or class.BaseCost
+		if currentState.Currencies.Gold < cost then
+			details.upgradeButton.Active = false
+			details.upgradeButton.ImageColor3 = Color3.fromHex("#3f3f3f")
+		else
+			details.upgradeButton.Active = true
+			details.upgradeButton.ImageColor3 = Color3.fromRGB(255, 255, 255)
+		end
+	end
 
 	-- Boat
 	if class.Entity == FFGEnum.CLASS.ENTITY_NAME.Boat then
@@ -152,11 +178,24 @@ if details.upgradeButton then details.upgradeButton.Activated:Connect(function()
 end) end
 
 function handleUpgradeClick()
-	-- Send event with class to say it has been upgraded
+	if currentState.Currencies and currentState.Currencies.Gold then
+		local cost = entityData.isPurchased and entityData.UpgradeCost or entityData.BaseCost
+		if currentState.Currencies.Gold < cost then print("Cannot afford") end
+	end
+
+	local events = {
+		Purchased = Events.GetRemote(Events.RemoteNames.EntityPurchased),
+		Upgraded = Events.GetRemote(Events.RemoteNames.EntityUpgraded),
+	}
+
+	if not entityData.isPurchased then
+		events.Purchased:FireServer(entityData[FFGEnum.CLASS.PROPERTIES.Id])
+	else
+		events.Upgraded:FireServer(entityData[FFGEnum.CLASS.PROPERTIES.Id])
+	end
+
 	-- write another listener for the server and client that handle upgrading
 	-- client sends update event
-	-- Server listens (teammanager i think) and upgrades correct class
+	-- Server listens (teammanager i think) and upgrades correct class as well as setting it to show/upgrade and startFishing
 	-- Server sends the latest class data for the UI to update
-
-	-- Note: RemoteFunction might be best for this
 end
